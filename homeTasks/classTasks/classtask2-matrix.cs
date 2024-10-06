6358e1fbdc723f93958d25f574481c8335a5f3a8
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-06 15:45:25 +0400

    added class tasks

---
+++ homeTasks/classTasks/classtask2-matrix.cs
@@ -0,0 +1,20 @@
+﻿int[,] numbers =
+{
+    {1,3,44,7 },
+    {7,9,11,5 },
+    {21,25,56,7 },
+    {34,6,8,26 }
+};
+int sum = 0;
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        if (i==j)
+        {
+            sum += numbers[i, j];
+        }
+    }
+
+}
+Console.WriteLine(sum);
---
+++ homeTasks/classTasks/classtask4--matrixcs.cs
@@ -0,0 +1,27 @@
+﻿int[,] numbers =
+{
+    {1,3,44,7 },
+    {7,9,11,5 },
+    {21,25,56,7 },
+    {34,6,8,26 }
+};
+int sumUp = 0;
+int sumDown = 0;
+int result=0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        if (i < j)
+        {
+            sumUp += numbers[i, j];
+        }else if (i > j)
+        {
+            sumDown += numbers[i, j];
+        }
+        result = sumUp - sumDown;
+    }
+}
+
+Console.WriteLine(result);
