dc2dee98ec0c8602cdcbada825e545d1848800ea
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-15 23:54:03 +0400

    Vehicle_car-class task done

---
+++ labTasks/15okt-classes/vehicleCar/vehicleCar/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace _06_Class_Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mercedes = new Car(2024, "Mercedes", "AMG GT 63 S", 65, 0.25, 35, "Black");
+            mercedes.ShowInfo();
+            mercedes.Drive(120);
+        }
+    }
+}
