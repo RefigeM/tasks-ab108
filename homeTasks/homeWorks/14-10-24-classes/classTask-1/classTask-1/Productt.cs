c24bb9a1dca13ab0dfbb7f31001a649fc02b9acb
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-15 20:18:27 +0400

    added classtask homework

---
+++ homeTasks/homeWorks/14-10-24-classes/classTask-1/classTask-1/Productt.cs
@@ -0,0 +1,18 @@
+﻿
+namespace classTask_1
+{
+    class Product
+    {
+        public string Name;
+        public string BrandName;
+        public double Price;
+
+        public Product(string name, string brandName, double price)
+        {
+            Name = name;
+            BrandName = brandName;
+            Price = price;
+
+        }
+    }
+}
---
+++ homeTasks/homeWorks/14-10-24-classes/classTask-1/classTask-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using classTask_1;
+
+namespace class_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product cay = new Product("Berqa", "Earl tea", 9.3d);
+            Product sud = new Product("Azer sud", "Tetra Pak", 1.89d);
+            Product salfet = new Product("Sun Salfet", "Maxi", 0.95d);
+            Product yogurt = new Product("Milla yogurt", "Milla", 0.45d);
+            Product makoron = new Product("Ankara makoron", "YUksuk", 3.6d);
+
+            Product[] allProducts = { cay, sud, salfet, yogurt, makoron };
+
+            productFilter(allProducts, 1.5, 3.6);
+        }
+        static void productFilter(Product[] products, double minPrice, double maxPrice)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Price <= maxPrice && product.Price >= minPrice)
+                {
+                    Console.WriteLine($"{product.Name}");
+                }
+            }
+        }
+    }
+}
