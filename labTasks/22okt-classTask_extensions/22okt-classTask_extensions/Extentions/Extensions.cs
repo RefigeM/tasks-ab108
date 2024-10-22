a2c2fa3567546771b40b366efe5a0c71e07b5853
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-23 00:59:13 +0400

    Added class task

---
+++ labTasks/22okt-classTask_extensions/22okt-classTask_extensions/Extentions/Extensions.cs
@@ -0,0 +1,68 @@
+﻿namespace _22okt_classTask_extensions.Extentions
+{
+    static class Extensions
+    {
+        public static bool IsOdd(this int number)
+        {
+            if (number % 2 == 0) return false;
+            return true;
+        }
+        public static bool IsEven(this int number)
+        {
+            if (number % 2 == 0) return true;
+            return false;
+        }
+
+        //for ile   
+        public static bool HasDigit(this string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i])) return true;
+                return false;
+            }
+            return false;
+
+        }
+        //foraech ile
+        public static bool HasDigit2(this string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c)) return true;
+            }
+            return false;
+
+        }
+
+        public static bool CheckPassword(this string text)
+        {
+            if (text.Length < 8) return false;
+
+            bool hasDigit = false;
+            bool hasUpper = false;
+            var hasLower = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i])) hasDigit = true;
+                if (char.IsUpper(text[i])) hasUpper = true;
+                if (char.IsLower(text[i])) hasLower = true;
+
+                if (hasDigit && hasUpper && hasLower) return true;
+            }
+
+            return false;
+
+        }
+        public static string Capitalize(this string word) {
+            return char.ToUpper(word[0]) + word.Substring(1).ToLower();
+            
+        
+        }
+
+
+
+    }
+}
+   
---
+++ labTasks/22okt-classTask_extensions/22okt-classTask_extensions/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using _22okt_classTask_extensions.Extentions;
+
+namespace _22okt_classTask_extensions
+{
+    public class Program
+    {
+        static string text = "123Refiqe";
+        static int number = 6;
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Extensions.IsOdd(6));
+            Console.WriteLine(number.IsOdd());
+
+            //Console.WriteLine(Extensions.IsEven(5));
+            Console.WriteLine(5.IsEven());
+
+            //Console.WriteLine(Extensions.HasDigit);
+            Console.WriteLine(text.HasDigit());
+            Console.WriteLine(text.HasDigit2());
+
+            Console.WriteLine(text.CheckPassword());
+            Console.WriteLine("sAlaMm".Capitalize());
+
+        }
+    }
+}
