4a1c3f0508da185ebbc9639fbfa83c1b2a92c5b6
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-24 01:22:21 +0400

    added indexer task

---
+++ homeTasks/homeWorks/23-10-24_indexer/23-10-24_indexer/ListInt.cs
@@ -0,0 +1,110 @@
+﻿namespace _23_10_24_indexer
+{
+    public class ListInt
+    {
+        private int[] array;
+
+        public ListInt(int size)
+        {
+            array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= array.Length)
+                    return 0;
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= array.Length) return;
+                array[index] = value;
+            }
+        }
+
+        public void Add(int num)
+        {
+            int[] newArray = new int[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            newArray[array.Length] = num;
+
+            array = newArray;
+        }
+        public void Add(params int[] nums)
+        {
+            int[] newArray2 = new int[array.Length + nums.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray2[i] = array[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    newArray2[array.Length + j] = nums[j];
+                }
+
+            }
+            array = newArray2;
+        }
+        public int Pop()
+        {
+            if (array.Length == 0) throw new InvalidOperationException("arrayda elemnt yoxdur."); ;
+
+
+            int lastElement = array[array.Length - 1];
+            int[] newArray3 = new int[array.Length - 1];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                newArray3[i] = array[i];
+            }
+            return lastElement;
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public int IndexOf(int num)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == num) return i;
+            }
+            return -1;
+            
+        }
+        public int LastIndexOf(int num)
+        {
+            for (int i = array.Length-1; i >=0; i--)
+            {
+                if (array[i] == num) return i;
+            }
+            return -1;
+        }
+        //public int Insert(int num, int index)
+        //{
+        //    int[] newArray4 = new int[array.Length +1];
+        //    for (int i = 0; i < array.Length; i++){
+        //        if (i == index) newArray4[i] = num;
+        //        newArray4[i]=array[i];  
+        //        }
+        //    array = newArray4;
+        //    return num;
+              
+            
+
+
+        //}
+
+    }
+}
+
---
+++ homeTasks/homeWorks/23-10-24_indexer/23-10-24_indexer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _23_10_24_indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ListInt myList = new ListInt(4);
+
+            myList[0] = 50;
+            myList[1] = 5;
+            myList[2] = 5;
+            myList[3] = 44;
+            myList[4] = 6;
+            myList[5] = 39;
+
+            myList.Add(34);
+
+            myList.Add(5, 6, 7);
+
+            Console.WriteLine(myList[4]);
+
+            Console.WriteLine(myList.Pop());
+            Console.WriteLine(myList.Sum());
+            Console.WriteLine(IndexOf(5));
+            Console.WriteLine(myList.IndexOf(20));
+            Console.WriteLine(myList.LastIndexOf(5));
+            //Console.WriteLine(myList.Insert(9,5));
+            var index = myList.IndexOf(9);
+            Console.WriteLine(index);
+
+        }
+    }
+}
