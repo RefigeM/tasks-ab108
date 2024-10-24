31798a8faf2edd0634466b70e8fdf1b5cd2e5afc
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-25 00:48:47 +0400

    added 24-10-24 task

---
+++ homeTasks/homeWorks/24-10-24_homeTask-lab/24-10-24_homeTask-lab/IStudentService.cs
@@ -0,0 +1,15 @@
+﻿namespace _24_10_24_homeTask_lab
+{
+    internal interface IStudentService
+    {
+       public Student GetStudentByID(int id);
+       public Student[] GetAllStudents();
+        public Student[] GetStudentsByName(string name);
+        void AddStudent(Student student);
+        void UpdateStudent(int id);
+        void RemoveStudent(int id, bool isSoftDelete);
+
+
+
+    }
+}
---
+++ homeTasks/homeWorks/24-10-24_homeTask-lab/24-10-24_homeTask-lab/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace _24_10_24_homeTask_lab
+{
+    public enum StudentStatus
+    {
+        Active,
+        Graduate,
+        Pending,
+        Removed
+
+    }
+    public class Student 
+    {
+        public static int counter { get; set; }
+        public  int Id { get; }
+        public Student()
+        {
+            Id=++counter;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public double GPA { get; set; }
+        public StudentStatus Status { get; set; }
+        public string Major { get; set; }
+
+
+    }
+}
