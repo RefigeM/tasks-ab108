d46cd5efec2756d664b2f7ab8f2781fb7b749a82
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-26 12:58:31 +0400

    weapon task done

---
+++ labTasks/WeapenTask-oop/WeapenTask-oop/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace WeapenTask_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapen weapon = new Weapen(50,40,20,"auto");
+         
+            int click = int.Parse(Console.ReadLine());
+            switch (click)
+            {
+                case 0:
+                    Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün\r\nqısayoldur.\r\n"); break;
+                case 1:
+                    weapon.Shoot();
+                    break;
+                case 2:
+                    weapon.Fire();
+                    break;
+                case 3:
+                    weapon.GetRemainBulletCount();
+                    break;
+                case 4:
+                    weapon.Reload();
+                    break;
+                case 5:
+                    weapon.ChangeFireMode();
+                    break;
+                case 6:
+                    Console.WriteLine("program dayandi");
+                    break;
+
+                default:
+                    Console.WriteLine("Duzgun code daxil edilmedi!\n");
+
+                    Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün\r\nqısayoldur.\r\n");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ labTasks/WeapenTask-oop/WeapenTask-oop/Weapen.cs
@@ -0,0 +1,106 @@
+﻿namespace WeapenTask_oop
+{
+    internal class Weapen
+    {
+        public int _capacityBullet;
+        public int CapacityBullet { get; set; }
+        public int _bulletCurrent;
+
+        public int BulletCurrent { get; set; }
+
+        public float _secondEndBullet;
+        public int SecondEndBullet { get; set; }
+        public string _fireMode;
+        public string FireMode { get; set; }
+
+
+        public void Shoot()
+        {
+            if (BulletCurrent<= CapacityBullet) 
+            {
+                if (BulletCurrent > 0)
+                {
+                    Console.WriteLine("1 gulle atildi.");
+                    BulletCurrent--;
+                    Console.WriteLine($"daraqda {BulletCurrent} sayda gulle qaldi.");
+                }
+                else
+                {
+                    Console.WriteLine("daraqda gulle yoxdur");
+                }
+            }
+        }
+        public void Fire()
+        {
+            if (BulletCurrent <= CapacityBullet)
+            {
+
+                if (FireMode == "auto")
+                {
+                    if (CapacityBullet != 0)
+                    {
+                        float bitmesan = BulletCurrent * SecondEndBullet / CapacityBullet;
+                        Console.WriteLine($"daraqdaki {BulletCurrent} sayda gulle {bitmesan} saniyede bitdi");
+                        BulletCurrent = 0;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("daraqda gulle yoxdur");
+                    }
+                    BulletCurrent = 0;
+                }
+                else
+                {
+                    Shoot();
+                }
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            if (BulletCurrent <= CapacityBullet)
+            {
+                Console.WriteLine($"daragin dolmasi ucun lazim olan gulle sayi{CapacityBullet - BulletCurrent}");
+            }
+            return CapacityBullet - BulletCurrent;
+
+
+
+        }
+        public int Reload()
+        {
+            if (BulletCurrent <= CapacityBullet) { 
+
+            BulletCurrent = BulletCurrent + CapacityBullet - BulletCurrent;
+            Console.WriteLine($"daraq tam dolduruldu,hazirki gulle sayi:{BulletCurrent}");
+        }
+            return BulletCurrent + CapacityBullet - BulletCurrent;
+
+
+        }
+        public string ChangeFireMode()
+        {
+            if (FireMode == "single")
+            {
+                FireMode = "auto";
+                Console.WriteLine("auto mod'a kecirildi");
+                return FireMode;
+
+            }
+            else
+            {
+                FireMode = "single";
+                Console.WriteLine("single mod'a kecirildi");
+
+            }
+            return FireMode;
+        }
+        public Weapen(int BulletCurrent, int CapacityBullet, int SecondEndBullet, string FireMode)
+        {
+            this.BulletCurrent = BulletCurrent;
+            this.CapacityBullet = CapacityBullet;
+            this.SecondEndBullet = SecondEndBullet;
+            this.FireMode = FireMode;
+        }
+    }
+}
