d512849e910b631529a59f7379725613836b971e
RefigeM <rafigam-ab108@gmail.edu.az>
2024-10-29 23:56:01 +0400

    added generic task

---
+++ homeTasks/homeWorks/29-10-24_generic/linkedList(opt.task)/linkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace linkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> myNumbers = new LinkedList<int>();
+            myNumbers.AddFirst(6);
+            var node = myNumbers.AddFirst(7);
+            myNumbers.AddLast(2);
+            myNumbers.AddAfter(node, 5);
+            myNumbers.AddFirst(1);
+
+            foreach (var item in myNumbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
